4d2e79bc1a09297688a2c4b83abedbd0e48e53c0
flashm123 <n_plk7a@mail.ru>
2021-03-17 01:59:29 +0300

    Add parallel for; add command line arguments handling; code review

--- src/FindFrequentTriplets/Program.cs
+++ src/FindFrequentTriplets/Program.cs
@@ -5,15 +5,22 @@ using System.Linq;
 using System.Threading;
 using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace FindFrequentTriplets
 {
-	class Program
-	{
-        private const int threadsCount = 3;
+    class Program
+    {
+        private const int partCount = 3;
 
-		public static void Main(string[] args)
-		{
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.Write("Please input the file path!");
+                return;
+            }
+            
             if (!File.Exists(args[0]))
             {
                 Console.Write("The file doesn't exist!");
@@ -26,188 +33,192 @@ namespace FindFrequentTriplets
             // Initialize stopwatch to measure text processing time
             var stopWatch = new Stopwatch();
             stopWatch.Start();
-            
+
             // Get 10 the most frequent triplets from the input text
-            var result = FindTripletsAsync(input);
+            var result = FindTripletsSync(input);
 
             stopWatch.Stop();
 
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 9)
+                {
+                    Console.WriteLine(string.Format("{0} - {1}", result[i].Letters, result[i].Frequency));
+                    break;
+                }
+                Console.Write(string.Format("{0}- {1} ", result[i].Letters, result[i].Frequency));
+            }
+
             Console.WriteLine(stopWatch.Elapsed);
+
+
+
+            var stopWatch1 = new Stopwatch();
+            stopWatch1.Start();
+
+            // Get 10 the most frequent triplets from the input text
+            var result1 = FindTripletsAsync(input);
+
+            stopWatch1.Stop();
 
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine(string.Format("{0} - {1}", result[i].Letters, result[i].Frequency));
+                if (i == 9)
+                {
+                    Console.WriteLine(string.Format("{0} - {1}", result[i].Letters, result[i].Frequency));
+                    break;
+                }
+                Console.Write(string.Format("{0}- {1} ", result[i].Letters, result[i].Frequency));
             }
 
+            Console.WriteLine(stopWatch1.Elapsed);
+
             Console.ReadKey();
-		}
+        }
 
-        public static void FindTripletsSync(string input)
-		{
-			var triplets = MakeTripletCombinations(PrepareText(input));
+        public static List<Triplet> FindTripletsSync(string input)
+        {
+            var triplets = MakeTripletCombinations(PrepareText(input));
 
-            // Initialize stopwatch to measure text processing time
-			var stopWatch = new Stopwatch();
-		    stopWatch.Start();
-			
-		    var result = ProcessText(triplets);
-			
-			stopWatch.Stop();
-			Console.WriteLine(stopWatch.Elapsed);
+            var result = ProcessText(triplets);
 
-            var groupedOrderedTriplets = result.OrderByDescending(groupedTriplet => groupedTriplet.Frequency)
-										       .ToList<Triplet>();
-			
-			for (int i = 0; i < 10; i++)
-			{
-				Console.WriteLine(string.Format("{0} - {1}", groupedOrderedTriplets[i].Letters, groupedOrderedTriplets[i].Frequency));
-			}
-			
-		}
+            return result.OrderByDescending(groupedTriplet => groupedTriplet.Frequency)
+                         .ToList<Triplet>();
+        }
 
         public static List<Triplet> FindTripletsAsync(string input)
-		{
-			var triplets = MakeTripletCombinations(PrepareText(input));
+        {
+            var triplets = MakeTripletCombinations(PrepareText(input));
 
             var tripleParts = SplitTripletsIntoParts(triplets);
 
-            return ProcessTripletsAsync(tripleParts, threadsCount)
+            return ProcessTripletsAsync(tripleParts)
                 .GroupBy(tr => tr.Letters,
                          triplet => triplet,
                         (key, value) => new Triplet() { Letters = key, Frequency = value.Sum(gr => gr.Frequency) })
                 .OrderByDescending(tr => tr.Frequency)
                 .ToList<Triplet>();
-		}
-		
-		/// <summary>
-		/// Get count of how many times the selected triple is in triple list
-		/// </summary>
-		/// <param name="triplets">Triplets list</param>
-		/// <param name="letters">Three letters to search</param>
-		/// <returns></returns>
-		public static int GetTripletFrequency(List<Triplet> triplets, string letters)
-		{
-			return triplets.Count(triplet => triplet.Letters == letters);
-		}
-		
-		/// <summary>
-		/// Prepares input text: remove whitespaces and non-letter symbols
-		/// </summary>
-		/// <param name="text">Text from file</param>
-		/// <returns>Modified text with letters only</returns>
-		private static string PrepareText(string text)
-		{
-			return string.Concat(text.Where(letter => (letter != ' ') && (char.IsLetter(letter))).Select(item => item.ToString())).ToLower();
-		}
-		
-		/// <summary>
-		/// Make triplet combinations from string of letters and keep them to triplets list
-		/// </summary>
-		/// <param name="source">Initial text with letters only</param>
-		public static List<Triplet> MakeTripletCombinations(string source)
-		{
-			var triplets = new List<Triplet>();
-			
-			for (int i = 0; i < source.Length - 2; i++)
-			{
-				triplets.Add(new Triplet(source[i].ToString() 
-				                         + source[i+1].ToString() 
-				                         + source[i+2].ToString()));
-			}
-			
-			return triplets;
-		}
+        }
 
         /// <summary>
-        /// Count how many triplet combinations appaear in the text: create unique triplets table - letters and frequency
+        /// Get count of how many times the selected triple is in the triple list
+        /// </summary>
+        /// <param name="triplets">Triplets list</param>
+        /// <param name="letters">Three letters to search</param>
+        /// <returns>The number of times the specified triplet is contained in the text</returns>
+        private static int GetTripletFrequency(List<Triplet> triplets, string letters)
+        {
+            return triplets.Count(triplet => triplet.Letters == letters);
+        }
+
+        /// <summary>
+        /// Prepares input text: remove whitespaces and non-letter symbols
+        /// </summary>
+        /// <param name="text">Text from file</param>
+        /// <returns>Modified text with letters only</returns>
+        private static string PrepareText(string text)
+        {
+            return string.Concat(text.Where(letter => (letter != ' ') && (char.IsLetter(letter))).Select(item => item.ToString())).ToLower();
+        }
+
+        /// <summary>
+        /// Make triplet combinations from string of letters and keep them to triplet list
+        /// </summary>
+        /// <param name="source">Initial text with letters only</param>
+        private static List<Triplet> MakeTripletCombinations(string source)
+        {
+            var triplets = new List<Triplet>();
+
+            for (int i = 0; i < source.Length - 2; i++)
+            {
+                triplets.Add(new Triplet(source[i].ToString()
+                                         + source[i + 1].ToString()
+                                         + source[i + 2].ToString()));
+            }
+
+            return triplets;
+        }
+
+        /// <summary>
+        /// Count how many triplet combinations appear in the text: create unique triplets table - letters and frequency
         /// </summary>
         /// <param name="triplets">All triplet</param>
         /// <returns>The triplets table - letters and frequency</returns>
-        public static List<Triplet> ProcessText(List<Triplet> triplets)
-		{
-			// Create a unique triplet table to store frequency here
+        private static List<Triplet> ProcessText(List<Triplet> triplets)
+        {
+            // Create a unique triplet table to store the frequency here
             var uniqueTriplets = triplets.GroupBy(tr => tr.Letters,
-                                                  triplet => triplet, 
+                                                  triplet => triplet,
                                                  (key, value) => new Triplet() { Letters = key, Frequency = value.First().Frequency })
                                          .ToList<Triplet>();
 
+            // Search how many times each unique triplet is contained in the whole triplet list
             for (int i = 0; i < uniqueTriplets.Count; i++)
-			{
+            {
                 var uniqueTriplet = uniqueTriplets[i];
                 uniqueTriplet.Frequency = GetTripletFrequency(triplets, uniqueTriplets[i].Letters);
 
                 uniqueTriplets[i] = uniqueTriplet;
-			}
-
-            return uniqueTriplets;
-		}
-		
-		/// <summary>
-		/// Split the tripl list into the several parts (part = thread)
-		/// </summary>
-		/// <param name="triplets">All triplet combinations</param>
-		/// <returns>List of triplet parts</returns>
-        private static List<List<Triplet>> SplitTripletsIntoParts(List<Triplet> triplets)
-		{
-			// parts = threads
-			const int partCount = threadsCount;
-			var partSize = triplets.Count / partCount;
-			var lastPartSize = triplets.Count - partSize * partCount + partSize;
-			
-			var tripleParts = new List<List<Triplet>>();
-			
-			for (int i = 0; i < partCount; i++)
-			{
-				tripleParts.Add(new List<Triplet>());
-				
-				if (i == partCount - 1)
-				{
-					for (int j = i * partSize; j < i * partSize + lastPartSize; j++)
-					{
-						tripleParts[i].Add(triplets[j]);
-					}
-                    break;
-				}
-				
-				for (int j = i * partSize; j < i * partSize + partSize; j++)
-				{
-					tripleParts[i].Add(triplets[j]);
-				}
-			}
-
-            return tripleParts;
-		}
-
-        /// <summary>
-        /// Perform text processing in multiple threads: each unique triplet part is processed by separate thread. Finally, the result parts unite.
-        /// </summary>
-        /// <param name="tripleParts">The unique triplet parts</param>
-        /// <param name="threadsCount">The count of the threads</param>
-        /// <returns>The result </returns>
-        private static List<Triplet> ProcessTripletsAsync(List<List<Triplet>> tripleParts, int threadsCount)
-        {
-            var results = new List<List<Triplet>>();
-            var threads = new Thread[threadsCount];
-            var output = new List<Triplet>();
-
-            for (int i = 0; i < threadsCount; i++)
-            {
-                threads[i] = new Thread(new ThreadStart(delegate()
-                {
-                    results.Add(new List<Triplet>());
-                    results[i] = ProcessText(tripleParts[i]);
-                }));
-
-                threads[i].Start();
-                threads[i].Join();
             }
 
-            foreach(var result in results)
+            return uniqueTriplets;
+        }
+
+        /// <summary>
+        /// Split the tripl list into the several parts
+        /// </summary>
+        /// <param name="triplets">All triplet combinations</param>
+        /// <returns>List of triplet parts</returns>
+        private static List<List<Triplet>> SplitTripletsIntoParts(List<Triplet> triplets)
+        {
+            var partSize = triplets.Count / partCount;
+            var lastPartSize = triplets.Count - partSize * partCount + partSize;
+
+            var tripleParts = new List<List<Triplet>>();
+
+            for (int i = 0; i < partCount; i++)
+            {
+                tripleParts.Add(new List<Triplet>());
+
+                if (i == partCount - 1)
+                {
+                    for (int j = i * partSize; j < i * partSize + lastPartSize; j++)
+                    {
+                        tripleParts[i].Add(triplets[j]);
+                    }
+                    break;
+                }
+
+                for (int j = i * partSize; j < i * partSize + partSize; j++)
+                {
+                    tripleParts[i].Add(triplets[j]);
+                }
+            }
+
+            return tripleParts;
+        }
+
+        /// <summary>
+        /// Perform text processing in parallel foreach: each unique triplet part is processed by separate thread. Finally, the received parts unite.
+        /// </summary>
+        /// <param name="tripleParts">The unique triplet parts</param>
+        /// <returns>The result</returns>
+        private static List<Triplet> ProcessTripletsAsync(List<List<Triplet>> tripleParts)
+        {
+            var results = new List<List<Triplet>>();
+            var output = new List<Triplet>();
+
+            Parallel.ForEach(tripleParts, triplePart =>
+            {
+                results.Add(ProcessText(triplePart));
+            });
+
+            foreach (var result in results)
             {
                 output.AddRange(result);
             }
 
             return output;
         }
-	}
+    }
 }
